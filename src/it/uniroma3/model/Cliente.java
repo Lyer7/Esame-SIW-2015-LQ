de14144861a67b1824beb62af5bd6370075f9f13
Lyer7 <andre.lin@hotmail.it>
2015-05-07 22:55:02 +0200

    small changes to class Cliente, made in order to see how Github commit.

--- src/it/uniroma3/model/Cliente.java
+++ src/it/uniroma3/model/Cliente.java
@@ -1,5 +1,49 @@
 package it.uniroma3.model;
 
+
+import javax.persistence.*;
+
+import java.util.*;
+
+@Entity
+@Table(name="Tabella_Clienti")
+@NamedQuery(name = "findAllClienti", query = "SELECT c FROM Cliente c")
 public class Cliente {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(nullable = false)
+    private String nome;
+
+    @Column(nullable = false)
+    private String cognome;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    @Temporal(TemporalType.DATE)
+    private Date dataDiNasciata;
+
+    @Column(nullable = false)
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date dataDiRegistrazione;
+
+    @OneToOne(cascade = {CascadeType.PERSIST, CascadeType.REMOVE})
+    private Indirizzo indirizzo;
+
+    @OneToMany(mappedBy = "cliente", fetch = FetchType.LAZY, cascade = {CascadeType.PERSIST, CascadeType.REMOVE})
+    private List<Ordine> ordini;
+
+    public Cliente() {}
+
+    public Cliente(String nome, String cognome, String email, Date dataDiNascita){
+    	this.nome = nome;
+    	this.cognome = cognome;
+    	this.email = email;
+    	this.dataDiNascita = dataDiNascita;
+    }
 
 }
